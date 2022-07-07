017f23a653a9a347cb18d4a24a86805f9117ee96
MartinPek <martin_pek@web.de>
2022-07-07 16:24:48 +0200

    removed unnecessary defines

--- BREAKOUT/include/header_st.h
+++ BREAKOUT/include/header_st.h
@@ -15,21 +15,6 @@ const uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 #define RFID_DATABLOCK      1
 #define OLED_I2C_ADD 0x3C    // Ist durch Hardware des OLEDs vorgegeben
 
-
-// --- RFID settings ---
-
-// define the pins for SPI communication. this may move to std lib
-#define PN532_SCK               13
-#define PN532_MOSI              11
-#define PN532_MISO              12
-
-#define RFID_1_SS_PIN           8     /* Per Konvention ist dies RFID-Port 1                                */
-#define RFID_2_SS_PIN           7     /* Per Konvention ist dies RFID-Port 2                                */
-#define RFID_3_SS_PIN           4     /* Per Konvention ist dies RFID-Port 3                                */
-#define RFID_4_SS_PIN           2     /* Per Konvention ist dies RFID-Port 4                                */
-
-const byte RFID_SSPins[]  = {RFID_1_SS_PIN};
-
 // --- LED settings --- 
 #define NR_OF_LEDS             31  /* Anzahl der Pixel auf einem Strang (Test 1 Pixel)                   */
 #define STRIPE_CNT             1

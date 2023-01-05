0e93023abdea5b18dbade4e274fecd8a9159861d
Seif elbouhy <seif.elbouhy@gmail.com>
2023-01-05 13:23:35 +0100

    remove unnecessary  imports
    add some logging and comments

--- TELEPHONE/src/telephone.py
+++ TELEPHONE/src/telephone.py
@@ -6,7 +6,9 @@ import pygame
 from pygame.locals import *
 import RPi.GPIO as GPIO
 from time import sleep
-import sys
+from server import *
+from threading import Thread
+
 
 '''
 =========================================================================================================
@@ -89,11 +91,30 @@ pygame.mixer.music.pause()
 
 '''
 =========================================================================================================
-Initialize size of code digits
+Initialize size of code digits, language and entered number
 =========================================================================================================
 '''
-maxNumberOfDigits = 10
+maxNumberOfDigits = 12
+language = "deu/"
+Number = ""
+'''
+=========================================================================================================
+Initialize a thread class
+=========================================================================================================
+'''
 
+class CustomThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None, daemon = None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+             
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
 
 '''
 =========================================================================================================
@@ -132,19 +153,63 @@ def checkingNumberSound(path):
 
 
 def checkNumber(Number):
-     logging.info(f"The Number {Number} is being checked ")
-     if Number == "0401234444":
+     
+     if Number == "86753489":
           pauseCurrentSound()
-          checkingNumberSound(config['PATH']['sounds'] + "SoundClip3.wav")
-     elif Number == "0401235554":
+          checkingNumberSound(config['PATH']['sounds'] + language + "TaxiGerst.wav")
+     elif Number == "071101232267":
           pauseCurrentSound()
-          checkingNumberSound(config['PATH']['sounds'] + "SoundClip4.wav")
-     elif Number == "0711456667":
+          checkingNumberSound(config['PATH']['sounds'] + language + "Albrecht.wav")
+     elif Number == "90011123":
           pauseCurrentSound()
-          checkingNumberSound(config['PATH']['sounds'] + "SoundClip5.wav")
+          checkingNumberSound(config['PATH']['sounds'] + language +"Accomplice.wav")
+     elif len(Number) < 12:
+          return
      else:
           pauseCurrentSound()
           checkingNumberSound(config['PATH']['sounds'] + "dialedWrongNumber.wav")
+
+def checkCorrectDigit(event):
+
+     global Number
+
+     pygame.mixer.music.pause()
+     empty_channel = pygame.mixer.Channel(1)
+
+     if(event.key == 48):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "ZERO.wav")
+          Number = Number + "0"
+     if(event.key == 49):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "ONE.wav")
+          Number = Number + "1"
+     if(event.key == 50):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "TWO.wav")
+          Number = Number + "2"
+     if(event.key == 51):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "THREE.wav")
+          Number = Number + "3"
+     if(event.key == 52):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "FOUR.wav")
+          Number = Number + "4"
+     if(event.key == 53):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "FIVE.wav")
+          Number = Number + "5"
+     if(event.key == 54):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "SIX.wav")
+          Number = Number + "6"
+     if(event.key == 55):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "SEVEN.wav")
+          Number = Number + "7"
+     if(event.key == 56):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "EIGHT.wav")
+          Number = Number + "8"
+     if(event.key == 57):
+          effect = pygame.mixer.Sound(config['PATH']['sounds'] + "NINE.wav")
+          Number = Number + "9"
+     
+     effect.set_volume(1)
+     empty_channel.play(effect)
+     pygame.time.delay(250)
 
 '''
 =========================================================================================================
@@ -154,15 +219,36 @@ Running the telephone
 
 def runSystem():
 
+     global language
+     global Number
+
      logging.info("Telephone now is running")
      # dialed number
-     Number = ""
      # time count for a player to press a key
      countTimer = 0
      # a flag to check that the key is pressed once
      keyPressedAtleastOnce = False
+     return_values = ""
+     thread = CustomThread(target=dataTransfer,daemon=True) 
+     try:
+          thread.start()    
+     except:
+          print("Failed to reopen the socket")
      
      while True: 
+
+          if not thread.is_alive():
+               # depending on the choice of the language the GM choose
+               if thread.join() == "GERMAN":
+                    language = "deu/"
+                    logging.info("The language now is german")
+               if thread.join() == "ENGLISH":
+                    language = "eng/"
+                    logging.info("The language now is english")
+
+               thread = None
+               thread = CustomThread(target=dataTransfer,daemon=True)
+               thread.start()
 
           # if the player takes the call unpause the beep sound and check the dialed number
           if GPIO.input(config["PIN"][city]["PHONE_switch_pin"]) == GPIO.LOW:
@@ -170,7 +256,7 @@ def runSystem():
                pygame.mixer.music.unpause()
 
                # countTimer reached 5s and player didn't press a button.
-               if len(Number) < 10 and countTimer == 50 :
+               if len(Number) < 12 and countTimer == 50 :
                     countTimer = 0
                     keyPressedAtleastOnce = False
                     logging.info("Player didn't press a button for 5s nor completed 10 digits")
@@ -178,7 +264,7 @@ def runSystem():
                     checkingNumberSound(config['PATH']['sounds'] + "dialedWrongNumber.wav")
                
                # check correct number
-               if len(Number) == 10:
+               if len(Number) == 12 or len(Number)==8:
                     checkNumber(Number)
 
                # keep on returning button state
@@ -192,69 +278,25 @@ def runSystem():
                          countTimer = 0
                     else : 
                          sleep(0.1)
-                         countTimer = countTimer +1
-                    
+                         countTimer = countTimer +1  
                if event.type == KEYDOWN :
-
                     keyPressedAtleastOnce = True
                     countTimer = 0
-
-                    pygame.mixer.music.pause()
-                    empty_channel = pygame.mixer.Channel(1)
-
-                    if(event.key == 48):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "ZERO.wav")
-                         Number = Number + "0"
-                    if(event.key == 49):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "ONE.wav")
-                         Number = Number + "1"
-                    if(event.key == 50):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "TWO.wav")
-                         Number = Number + "2"
-                    if(event.key == 51):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "THREE.wav")
-                         Number = Number + "3"
-                    if(event.key == 52):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "FOUR.wav")
-                         Number = Number + "4"
-                    if(event.key == 53):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "FIVE.wav")
-                         Number = Number + "5"
-                    if(event.key == 54):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "SIX.wav")
-                         Number = Number + "6"
-                    if(event.key == 55):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "SEVEN.wav")
-                         Number = Number + "7"
-                    if(event.key == 56):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "EIGHT.wav")
-                         Number = Number + "8"
-                    if(event.key == 57):
-                         effect = pygame.mixer.Sound(config['PATH']['sounds'] + "NINE.wav")
-                         Number = Number + "9"
-
-                    effect.set_volume(1)
-                    empty_channel.play(effect)
-                    pygame.time.delay(250)
-
-
+                    checkCorrectDigit(event)
                else:
                     pass
 
-
           else:
-             
                keyPressedAtleastOnce = False #reset flag of pressing the button at least once
                pygame.mixer.music.pause() #pause beep sound
                Number ="" # reset dialed number
                pygame.event.clear() #clear any button pressed after 10 digits
                
              
-
 def main():
+     # by default the language is german
+     language = "deu/"
      runSystem()
      
-
-if __name__ == '__main__': 
-     
+if __name__ == '__main__':  
      main()

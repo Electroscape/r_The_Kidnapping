32618b10b34923c9bd885e3994657369aa37c84b
MartinPek <martin.pek@web.de>
2023-08-17 17:10:36 +0200

    waterlevels: added progressive pumpbevhour and extraction effect

--- m_waterLevels/src/waterLevels.cpp
+++ m_waterLevels/src/waterLevels.cpp
@@ -25,12 +25,71 @@ STB_MOTHER_IO MotherIO;
 
 int lastState = -1;
 bool level1_active = false;
+bool level3_active = false;
+
+unsigned long pumpCycleSwitch = millis();
+
+int pumpPhase[2] = {0};
+int pumpIntensity = 0;
 
 
 void enableWdt() {
     wdt_enable(WDTO_8S);
 }
 
+
+void updatePumps() {
+
+    if (pumpIntensity == 4) { return; }
+
+    if (millis() > pumpCycleSwitch) {
+        if (pumpPhase[0] == 0) {
+            Mother.motherRelay.digitalWrite(pump_1, closed);
+            Mother.motherRelay.digitalWrite(pump_2, closed);
+        } else {
+            Mother.motherRelay.digitalWrite(pump_1, open);
+            Mother.motherRelay.digitalWrite(pump_2, open);
+        }
+        switch (pumpIntensity) {
+            case 0:
+                // 0 = false
+                if (pumpPhase[0] == 0) {
+                    pumpCycleSwitch = millis() + (unsigned long) 6000;
+                } else {
+                    pumpCycleSwitch = millis() + (unsigned long) 200;
+                }
+            break;
+            case 1:
+                if (pumpPhase[0] == 0) {
+                    pumpCycleSwitch = millis() + (unsigned long) 2000;
+                } else {
+                    pumpCycleSwitch = millis() + (unsigned long) 350;
+                }
+            break;
+
+            case 2:
+                if (pumpPhase[0] == 0) {
+                    pumpCycleSwitch = millis() + (unsigned long) 50;
+                } else {
+                    pumpCycleSwitch = millis() + (unsigned long) 800;
+                }
+            break;
+
+            case 3:
+                if (pumpPhase[0] == 0) {
+                    pumpCycleSwitch = millis() + (unsigned long) 400;
+                } else {
+                    pumpCycleSwitch = millis() + (unsigned long) 500;
+                }
+            break;
+        }
+        if (pumpPhase[0]) {
+            pumpPhase[0] = 0;
+        } else {
+            pumpPhase[0] = 1;
+        }
+    }
+}
 
 
 void handleInputs() {
@@ -40,6 +99,7 @@ void handleInputs() {
     if (lastState == result) {
         return;
     }
+
     lastState = result;
     Serial.println(result);
 
@@ -49,23 +109,39 @@ void handleInputs() {
 
     if (level_1 & result) {
         if (!level1_active) {
-            LED_CMDS::setAllStripsToClr(Mother, ledBrain, LED_CMDS::clrBlack, 100);
-            Mother.motherRelay.digitalWrite(pump_1, closed);
-            Mother.motherRelay.digitalWrite(pump_2, closed);
+            LED_CMDS::setAllStripsToClr(Mother, ledBrain, LED_CMDS::clrRed, 20);
+            pumpIntensity = 0;
+            pumpCycleSwitch = millis();
             level1_active = true;
+            level3_active = false;
             Mother.motherRelay.digitalWrite(lightTable, closed);
         }
     } else if (level1_active) {
+        pumpIntensity = 1;
+        pumpCycleSwitch = millis();
         level1_active = false;
-        LED_CMDS::setLEDToClr(Mother, ledBrain, LED_CMDS::clrRed, 100, 1, 0);
+        LED_CMDS::fade2color(Mother, ledBrain, LED_CMDS::clrRed, 20, LED_CMDS::clrRed, 60, 3000, 1);
+        // LED_CMDS::setLEDToClr(Mother, ledBrain, LED_CMDS::clrRed, 100, 1, 0);
     }
    
 
     if (level_2 & result) {
-        LED_CMDS::setLEDToClr(Mother, ledBrain, LED_CMDS::clrGreen, 100, 1, 1);
+        pumpIntensity = 2;
+        pumpCycleSwitch = millis();
+        LED_CMDS::fade2color(Mother, ledBrain, LED_CMDS::clrRed, 60, LED_CMDS::clrPurple, 80, 3000, 1);
     }
-    if (level_3 & result) {
-        LED_CMDS::setAllStripsToClr(Mother, ledBrain, LED_CMDS::clrPurple, 100);
+    if (level_3 & result && !level3_active) {
+        LED_CMDS::fade2color(Mother, ledBrain, LED_CMDS::clrPurple, 80, LED_CMDS::clrWhite, 20, 1500, 1);
+        pumpIntensity = 3;
+        pumpCycleSwitch = millis();
+        level3_active = true;
+    } else if (level3_active) {
+        level3_active = false;
+        pumpCycleSwitch = millis();
+        LED_CMDS::fade2color(Mother, ledBrain, LED_CMDS::clrWhite, 20, LED_CMDS::clrWhite, 5, 5000, 1);
+        Mother.motherRelay.digitalWrite(pump_1, closed);
+        Mother.motherRelay.digitalWrite(pump_2, closed);
+        pumpIntensity = 4;
     }
 }
 
@@ -88,6 +164,7 @@ void setup() {
 
 void loop() {
     handleInputs();    
+    updatePumps();
     wdt_reset();
 }
 

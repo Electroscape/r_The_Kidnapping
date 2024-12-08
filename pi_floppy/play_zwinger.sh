6e52ca151812f9218f316f385449611ecaf7bebb
Abdullah <abdullahsaei@hotmail.com>
2024-12-08 21:19:15 +0100

    floppy: fix pkill chromium

--- pi_floppy/play_zwinger.sh
+++ pi_floppy/play_zwinger.sh
@@ -1,10 +1,8 @@
 export DISPLAY=:0.0
-sudo pkill chromium
-sudo pkill vlc
 
 # cd to this script dir
 # fixes activate venv from crontab 
 cd "${0%/*}"
 
-cvlc --fullscreen --no-video-title --video-on-top --gain=1.0 --quiet --loop static/vids/zwinger_CCTV.mp4 &
+cvlc --fullscreen --no-video-title --video-on-top --gain=1.0 --quiet static/vids/zwinger_CCTV.mp4 &
 echo "zwinger video played, bye"

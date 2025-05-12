24457b449c23ca4134b4f79bc2f21fdb472085ab
ASmac <abdullah@pause-play.de>
2025-05-12 17:17:34 +0200

    pi_windows: add audio to the videos

--- pi_windows/video_trigger.py
+++ pi_windows/video_trigger.py
@@ -31,7 +31,7 @@ def main():
     while True:
         if arbiter_socket.read_buffer():
             os.system('pkill vlc')
-            os.system("DISPLAY=:0.0 cvlc --fullscreen --loop --no-video-title --no-audio ~/KDN-Videos/viewR_long.mp4 &")
+            os.system("DISPLAY=:0.0 cvlc --fullscreen --loop --no-video-title ~/KDN-Videos/viewR_long.mp4 &")
 
     # Deprecated use VLC > mplayer
     #  os.system("DISPLAY=:0.0 mplayer -fs -loop 0 -xineramascreen 0 KDN-Videos/viewL_long.mp4 &")
